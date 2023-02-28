ecde441530d7e4624ee735c947d1bf17d1db5cb6
xlat-code <googtor@gmail.com>
2023-02-28 16:34:42 +0300

    version:[2023-02-28]

---
+++ swar/config.h
@@ -0,0 +1,32 @@
+﻿#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "mylib.h"
+
+///------------------------------------|
+/// Правило на состав флотилии.        |
+/// Размер -> кол-во лодок.            |
+/// Число  -> кол-во палуб в лодке.    |
+///------------------------------------:
+struct  Cfg
+{       Cfg()
+        {
+            ///------------------------------|
+            /// Сначала с большим кол. палуб.|
+            ///------------------------------:
+            if(!std::is_sorted(std::begin(rule), std::end(rule)))
+            {   std::   sort  (std::begin(rule), std::end(rule),
+                               std::greater<int>());
+            }
+        };
+
+    size_t C = 12,
+           R = 12;
+
+    std::vector<size_t> rule = {5, 4,4, 3,3,3, 2,2,2,2, 1,1,1,1,1};
+  //std::vector<size_t> rule = {5, 4,4,4, 1,1};
+};
+
+inline Cfg cfg;
+
+#endif // CONFIG_H
---
+++ swar/mylib.h
@@ -0,0 +1,74 @@
+﻿#ifndef MYLIB_H
+#define MYLIB_H
+
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <list>
+
+#define ASSERT(u) if(!(u))  \
+                  std::cout << myl::to_string("ASSERT: -> line: ", __LINE__)\
+                            << '\n'
+#define ERROR_(v)  myl::to_string("ERROR: ", v, " -> line: ", __LINE__)
+#define l(v)      std::cout << #v << " = " << v << "\n";
+
+namespace std
+{
+    std::string to_string(const char*       s) { return s; }
+    std::string to_string(const std::string s) { return s; }
+}
+
+#include <ctime>
+#include <cstdlib>
+///---------|
+/// my lib  |
+///---------:
+namespace myl
+{
+    template <typename... TT>
+    std::string to_string(TT&& ... a)
+    {   return (std::to_string(a) + ...) + "\n";
+    }
+
+    template <typename ... TT>
+    void BANNER(TT&&   ... a )
+    {   ((std::wcout   <<  a << std::endl), ...);
+    }
+
+    static int rrand(int range_min, int range_max)
+    {   return rand() % (range_max - range_min) + range_min;
+    }
+
+    template <typename T>
+    void info(const T& m)
+    {   for(const auto&  row : m)
+        {   std::cout << row << '\n';
+        }   std::cout        << '\n';
+    }
+}
+
+inline void testfoo_rrand()
+{
+    for(int    i = 0; i < 100; ++i)
+    {   auto   r = myl::rrand(0, 5);
+        auto   b = 0 <= r && r < 5;
+        ASSERT(b);
+        if   (!b) return;
+    }
+    std::cout << __FUNCTION__ << " success!\n";
+}
+
+struct Point2i
+{   size_t x, y;
+
+    bool operator==(const Point2i& p) const { return p.x == x && p.y == y; }
+
+    void debug() const
+    {   std::cout << "{" << x << ", " << y << "}\n";
+    }
+};
+
+#endif // MYLIB_H
